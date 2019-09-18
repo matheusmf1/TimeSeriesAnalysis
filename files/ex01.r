a747876ff4deedda0e2eb2aa79d6c90dc485db90
Matheus <mathew.mfranco@gmail.com>
2019-09-18 20:25:52 -0300

    Remove untracked files

--- files/ex01.r
+++
@@ -1,9 +0,0 @@
-# Make a time series from min temperature
-D = read.csv2("colombo.csv")
-S = ts(D$temp_min,start = 1,end = 1368,frequency = 1)
-
-plot(S, main = "Colombo: Jun-Ago/2019", xlab = "Coletas", ylab = "Temperaturas")
-
-max = max(S)
-min = min(S)
-media = mean(S)
